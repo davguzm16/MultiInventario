e12f48fc07f465cbedbf082a98f1d1d03e33a068
davguzm16 <guzman.davi16@gmail.com>
2025-01-30 10:51:45 -0500

    Creacion de secciones login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:multiinventario/pages/login_page.dart';
+import 'package:multiinventario/pages/login/login_page.dart';
 
 void main() {
   runApp(const MainApp());
